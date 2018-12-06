8c4de347514337d35c11339341077392c6c8a446
Alex S <ags12432@gmail.com>
2018-12-06 04:07:00 -0500

    updated Workout model to include date

--- models/Workout.js
+++ models/Workout.js
@@ -13,6 +13,10 @@ const WorkoutSchema = new Schema({
     description: {
         type: String,
         required: true
+    },
+    date: {
+        type: Date,
+        default: Date.now
     }
 });
 
